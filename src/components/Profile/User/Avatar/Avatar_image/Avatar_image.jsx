18a24590b4351d03cced74c80c8976692c4149c8
nym-tmn <nymtmn8690@gmail.com>
2023-01-11 00:06:34 +0500

    Add new components for SocialNetworks and AvatarImage

---
+++ src/components/Profile/User/Avatar/Avatar_image/Avatar_image.jsx
@@ -0,0 +1,12 @@
+
+import classes from './../Avatar_image/Avatar_image.module.css';
+
+const AvatarImage = () => {
+	return (
+		<div className={classes.avatarContiner}>
+			<div className={classes.avatarImage}></div>
+		</div>
+	);
+};
+
+export default AvatarImage;
