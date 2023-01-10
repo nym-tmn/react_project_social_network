74f367d09af9c09e47ccc04d2082ab4903db7a21
nym-tmn <nymtmn8690@gmail.com>
2023-01-10 23:56:05 +0500

    Add new components for Avatar CoverPicture User Information

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,56 +1,13 @@
 
+import User from './User/User';
 import Environment from './Environment/Environment';
 
 import classes from './Profile.module.css';
-import socNet from './Profile_social_networks.module.css';
 
 const Profile = () => {
 	return (
 		<div className={classes.mainContent}>
-			<div className={classes.user}>
-				<div className={classes.coverPicture}></div>
-				<div className={classes.avatar}>
-					<div className={classes.avatarContiner}>
-						<div className={classes.avatarImage}></div>
-					</div>
-					<div className={socNet.socialNetworks}>
-						<div className={socNet.instagram}>
-							<a href="#s">
-								<img src="/img/icon_instagram.png" alt="icon_instagram.png" />
-							</a>
-							<a href="#s">
-								<div className={socNet.title}>www.instagram.com/john_smith</div>
-							</a>
-						</div>
-						<div className={socNet.twitter}>
-							<a href="#s">
-								<img src="/img/icon_twitter.png" alt="icon_twitter.png" />
-							</a>
-							<a href="#s">
-								<div className={socNet.title}>www.twitter.com/john_smith</div>
-							</a>
-						</div>
-						<div className={socNet.facebook}>
-							<a href="#s">
-								<img src="/img/icon_facebook.png" alt="icon_facebook.png" />
-							</a>
-							<a href="#s">
-								<div className={socNet.title}>www.facebook.com/john_smith</div>
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className={classes.information}>
-					<div className={classes.name}>John Smith</div>
-					<div className={classes.dateOfBirth}>Date of Birth: December 08,1987</div>
-					<div className={classes.city}>City: New York</div>
-					<div className={classes.education}>Education: New York University (NYU)'08</div>
-					<div className={classes.major}>Major: Communication and Journalism</div>
-					<div className={classes.hobby}>Hobby: Photography</div>
-					<div className={classes.mobilePhone}>Mobile phone: +1 212 555 9637</div>
-					<div className={classes.aboutMe}>About me: Lorem ipsum  corrupti id repudianlendus veniam nihil! Temporibus eveniet soluta repellat sequi ipsam eligendi nam amet .</div>
-				</div>
-			</div>
+			<User />
 			<Environment />
 		</div>
 	);
