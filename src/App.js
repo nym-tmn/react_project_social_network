c801940e70dff8510d74c32a4510e146f35acac2
nym-tmn <nymtmn8690@gmail.com>
2023-01-17 23:27:35 +0500

    Add new propses for MyPosts, add likes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
-
-
 
 const App = () => {
 	return (
--- src/components/Profile/Environment/My_posts/Posts/Posts.jsx
+++ src/components/Profile/Environment/My_posts/Posts/Posts.jsx
@@ -1,16 +1,32 @@
 
-import Post1 from './Post_1/Post_1';
-import Post2 from './Post_2/Post_2';
-import Post3 from './Post_3/Post_3';
+import Post from './Post/Post';
 
 import classes from './Posts.module.css';
 
 const Posts = () => {
 	return (
-		<div className={classes.post}>12
-			<Post1/>
-			<Post2/>
-			<Post3/>
+		<div className={classes.posts}>
+			<Post
+				postIconAvatar={require('./../../../../images/icon_avatar.png')}
+				postUserName='John Smith'
+				postText='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, ipsa quidem dolor adipisci doloribus eum. Lorem ipsum dolor sit amet consectetur 
+							adipisicing elit. Sed provident deserunt autem ab architecto aliquam ipsa dolorem, officiis inventore ratione obcaecati accusantium, ex et adipisci rerum iusto 
+							dolor quas debitis incidunt voluptatibus? Quas ea quae non omnis molestias ducimus possimus!'
+				postImage={require('./../../../../images/post1.png')}
+				likesCounter='15' />
+			<Post
+				postIconAvatar={require('./../../../../images/icon_avatar.png')}
+				postUserName='John Smith'
+				postText='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, ipsa quidem dolor adipisci doloribus eum.'
+				postImage={require('./../../../../images/post2.png')}
+				likesCounter='89' />
+			<Post
+				postIconAvatar={require('./../../../../images/icon_avatar.png')}
+				postUserName='John Smith'
+				postText='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, ipsa quidem dolor adipisci doloribus eum. Lorem ipsum dolor sit amet consectetur 
+							adipisicing elit. Neque fugiat quis commodi placeat, cum nostrum consequuntur exercitationem fugit sunt distinctio.'
+				postImage={require('./../../../../images/post3.png')}
+				likesCounter='392' />
 		</div>
 	);
 };
