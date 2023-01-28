fe578b9b09abe97c52d4cbf480f470f452b377b4
nym-tmn <nymtmn8690@gmail.com>
2023-01-28 16:01:24 +0500

    Used git revert for come back to before add components for Dialogs

--- src/components/Dialogs/Dialogs_items/Dialogs_items.jsx
+++ src/components/Dialogs/Dialogs_items/Dialogs_items.jsx
@@ -1,24 +1,35 @@
 
 import React from 'react';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dialog1 from './Dialog_1/Dialog_1';
-import Dialog2 from './Dialog_2/Dialog_2';
-import Dialog3 from './Dialog_3/Dialog_3';
-import Dialog4 from './Dialog_4/Dialog_4';
-import Dialog5 from './Dialog_5/Dialog_5';
-import Dialog6 from './Dialog_6/Dialog_6';
 
 import classes from './Dialogs_items.module.css';
 
 const DialogsItems = (props) => {
 	return (
 		<div className={classes.dialogsItems}>
-			<Dialog1 />
-			<Dialog2 />
-			<Dialog3 />
-			<Dialog4 />
-			<Dialog5 />
-			<Dialog6 />
+			<div className={`${classes.dialog} ${classes.activeDialog}`}>
+				<div className={classes.avatar}></div>
+				<div className={classes.name}><a href='#s'>Robert_lastName</a></div>
+			</div>
+			<div className={classes.dialog}>
+				<div className={classes.avatar}></div>
+				<div className={classes.name}><a href='#s'>Sophia_lastName</a></div>
+			</div>
+			<div className={classes.dialog}>
+				<div className={classes.avatar}></div>
+				<div className={classes.name}><a href='#s'>Alex_lastName</a></div>
+			</div>
+			<div className={classes.dialog}>
+				<div className={classes.avatar}></div>
+				<div className={classes.name}><a href='#s'>Andrew_lastName</a></div>
+			</div>
+			<div className={classes.dialog}>
+				<div className={classes.avatar}></div>
+				<div className={classes.name}><a href='#s'>John_lastName</a></div>
+			</div>
+			<div className={classes.dialog}>
+				<div className={classes.avatar}></div>
+				<div className={classes.name}><a href='#s'>Mary_lastName</a></div>
+			</div>
 		</div>
 	);
 }
