bae30b14cf5fa32ff9fcc8b1492310c64335f9da
nym-tmn <nymtmn8690@gmail.com>
2023-01-29 12:48:12 +0500

    Add props for MessageToMe and refactoring it

---
+++ src/components/Dialogs/Messages/Message_to_me/Message_to_me.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import classes from './Message_to_me.module.css';
+
+const MessageToMe = (props) => {
+	return (
+		<div className={classes.messageToMe}>
+			<div><span className={classes.message}>{props.message}</span></div>
+		</div>
+	);
+}
+
+export default MessageToMe;
--- src/components/Dialogs/Messages/Messages.jsx
+++ src/components/Dialogs/Messages/Messages.jsx
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import MessageToMe from './Message_to_me/Message_to_me';
+import MessageFromMe from './Message_from_me/Message_from_me';
 import SendMessages from './Send_messages/Send_messages';
 
 import classes from './Messages.module.css';
@@ -8,16 +10,12 @@ const Messages = (props) => {
 	return (
 		<div className={classes.messages}>
 			<div className={classes.messagesToMe}>
-				<div><span className={`${classes.message} ${classes.toMe}`}>Hi!</span></div>
-				<div><span className={`${classes.message} ${classes.toMe}`}>How are you?</span></div>
-				<div><span className={`${classes.message} ${classes.toMe}`}>What are you doing? Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestias odit fugiat facere id quae voluptatem voluptatibus ullam repudiandae recusandae, praesentium numquam voluptas quisquam sed ducimus amet dolorem architecto soluta eveniet, quasi eos, sapiente perferendis deserunt. Consequatur soluta ipsam, voluptate exercitationem, fuga quam, corporis laudantium nulla itaque perferendis ipsum maxime?</span></div>
+				<MessageToMe message='Hi!' />
+				<MessageToMe message='How are you?' />
+				<MessageToMe message='What are you doing? Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestias odit fugiat facere id quae voluptatem voluptatibus ullam repudiandae recusandae, praesentium numquam voluptas quisquam sed ducimus amet dolorem architecto soluta eveniet, quasi eos, sapiente perferendis deserunt. Consequatur soluta ipsam, voluptate exercitationem, fuga quam, corporis laudantium nulla itaque perferendis ipsum maxime?' />
 			</div>
-			<div className={classes.messagesFromMe}>
-				<div><span className={`${classes.message} ${classes.fromMe}`}>Yo!</span></div>
-				<div><span className={`${classes.message} ${classes.fromMe}`}>I'm fine!</span></div>
-				<div><span className={`${classes.message} ${classes.fromMe}`}>I'm going for a walk. Are you with me? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi maxime adipisci incidunt cumque ratione corporis recusandae, iure nihil quasi optio nobis autem maiores atque reiciendis. Debitis ut, voluptate neque dolor laborum est distinctio assumenda eum, quaerat nesciunt repellendus expedita provident culpa vero veniam repellat. Magnam quo vitae reiciendis dignissimos.</span></div>
-			</div>
-			<SendMessages/>
+			<MessageFromMe />
+			<SendMessages />
 		</div>
 	);
 }
