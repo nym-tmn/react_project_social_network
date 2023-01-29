d4738a416642b3fb4ed38e820f4568a216a5585c
nym-tmn <nymtmn8690@gmail.com>
2023-01-29 12:57:28 +0500

    Add props for MessageFromMe

--- src/components/Dialogs/Messages/Message_from_me/Message_from_me.jsx
+++ src/components/Dialogs/Messages/Message_from_me/Message_from_me.jsx
@@ -5,10 +5,8 @@ import classes from './Message_from_me.module.css';
 
 const MessageFromMe = (props) => {
 	return (
-		<div className={classes.messagesFromMe}>
-			<div><span className={`${classes.message} ${classes.fromMe}`}>Yo!</span></div>
-			<div><span className={`${classes.message} ${classes.fromMe}`}>I'm fine!</span></div>
-			<div><span className={`${classes.message} ${classes.fromMe}`}>I'm going for a walk. Are you with me? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi maxime adipisci incidunt cumque ratione corporis recusandae, iure nihil quasi optio nobis autem maiores atque reiciendis. Debitis ut, voluptate neque dolor laborum est distinctio assumenda eum, quaerat nesciunt repellendus expedita provident culpa vero veniam repellat. Magnam quo vitae reiciendis dignissimos.</span></div>
+		<div className={classes.messageFromMe}>
+			<div><span className={classes.message}>{props.message}</span></div>
 		</div>
 	);
 }
--- src/components/Dialogs/Messages/Messages.jsx
+++ src/components/Dialogs/Messages/Messages.jsx
@@ -14,7 +14,11 @@ const Messages = (props) => {
 				<MessageToMe message='How are you?' />
 				<MessageToMe message='What are you doing? Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestias odit fugiat facere id quae voluptatem voluptatibus ullam repudiandae recusandae, praesentium numquam voluptas quisquam sed ducimus amet dolorem architecto soluta eveniet, quasi eos, sapiente perferendis deserunt. Consequatur soluta ipsam, voluptate exercitationem, fuga quam, corporis laudantium nulla itaque perferendis ipsum maxime?' />
 			</div>
-			<MessageFromMe />
+			<div className={classes.messagesFromMe}>
+				<MessageFromMe message='Yo!' />
+				<MessageFromMe message='I am fine!' />
+				<MessageFromMe message='I am going for a walk. Are you with me? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi maxime adipisci incidunt cumque ratione corporis recusandae, iure nihil quasi optio nobis autem maiores atque reiciendis. Debitis ut, voluptate neque dolor laborum est distinctio assumenda eum, quaerat nesciunt repellendus expedita provident culpa vero veniam repellat. Magnam quo vitae reiciendis dignissimos.' />
+			</div>
 			<SendMessages />
 		</div>
 	);
