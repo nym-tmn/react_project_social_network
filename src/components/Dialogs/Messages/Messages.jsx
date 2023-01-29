37933db6666be3ba299f850ecd85267ba259c8e7
nym-tmn <nymtmn8690@gmail.com>
2023-01-29 14:29:39 +0500

    Add BLL or data for component Messages

--- src/components/Dialogs/Messages/Messages.jsx
+++ src/components/Dialogs/Messages/Messages.jsx
@@ -7,17 +7,31 @@ import SendMessages from './Send_messages/Send_messages';
 import classes from './Messages.module.css';
 
 const Messages = (props) => {
+
+	let messagesToMeData = [
+		{ id: 1, message: 'Hi!'},
+		{ id: 2, message: 'How are you?'},
+		{ id: 3, message: 'What are you doing? Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestias odit fugiat facere id quae voluptatem voluptatibus ullam repudiandae recusandae, praesentium numquam voluptas quisquam sed ducimus amet dolorem architecto soluta eveniet, quasi eos, sapiente perferendis deserunt. Consequatur soluta ipsam, voluptate exercitationem, fuga quam, corporis laudantium nulla itaque perferendis ipsum maxime?'},
+	];
+
+	let messagesFromMeData = [
+		{ id: 1, message: 'Yo!' },
+		{ id: 2, message: 'I am fine!' },
+		{ id: 3, message: 'I am going for a walk. Are you with me? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi maxime adipisci incidunt cumque ratione corporis recusandae, iure nihil quasi optio nobis autem maiores atque reiciendis. Debitis ut, voluptate neque dolor laborum est distinctio assumenda eum, quaerat nesciunt repellendus expedita provident culpa vero veniam repellat. Magnam quo vitae reiciendis dignissimos.' },
+	];
+
 	return (
 		<div className={classes.messages}>
 			<div className={classes.messagesToMe}>
-				<MessageToMe message='Hi!' />
-				<MessageToMe message='How are you?' />
-				<MessageToMe message='What are you doing? Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestias odit fugiat facere id quae voluptatem voluptatibus ullam repudiandae recusandae, praesentium numquam voluptas quisquam sed ducimus amet dolorem architecto soluta eveniet, quasi eos, sapiente perferendis deserunt. Consequatur soluta ipsam, voluptate exercitationem, fuga quam, corporis laudantium nulla itaque perferendis ipsum maxime?' />
+				<MessageToMe message={messagesToMeData [0].message} />
+				<MessageToMe message={messagesToMeData [1].message} />
+				<MessageToMe message={messagesToMeData [2].message} />
+				
 			</div>
 			<div className={classes.messagesFromMe}>
-				<MessageFromMe message='Yo!' />
-				<MessageFromMe message='I am fine!' />
-				<MessageFromMe message='I am going for a walk. Are you with me? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi maxime adipisci incidunt cumque ratione corporis recusandae, iure nihil quasi optio nobis autem maiores atque reiciendis. Debitis ut, voluptate neque dolor laborum est distinctio assumenda eum, quaerat nesciunt repellendus expedita provident culpa vero veniam repellat. Magnam quo vitae reiciendis dignissimos.' />
+				<MessageFromMe message={messagesFromMeData[0].message} />
+				<MessageFromMe message={messagesFromMeData[1].message} />
+				<MessageFromMe message={messagesFromMeData[2].message} />
 			</div>
 			<SendMessages />
 		</div>
