0a62f48d3b2091a57a71448c16ce355933ce0771
nym-tmn <nymtmn8690@gmail.com>
2023-03-08 00:49:35 +0500

    Create first class component SearchUsersItems

--- src/components/Header/Search_users/Search_users_items_container/Search_users_items/Search_users_items.jsx
+++ src/components/Header/Search_users/Search_users_items_container/Search_users_items/Search_users_items.jsx
@@ -5,38 +5,39 @@ import searchUserAvatar from './../../../../../assets/images/search_user_avatar.
 
 import classes from './Search_users_items.module.css';
 
-const SearchUsersItems = (props) => {
+class SearchUsersItems extends React.Component {
 
-	let getUsers = () => {
+	constructor(props) {
+		super(props);
+		alert('New')
+		axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+			this.props.setUsers(response.data.items);
+		});
 
-		if (props.usersData.length === 0) {
-
-			axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-				props.setUsers(response.data.items);
-			});
-		};
 	};
 
-	return (
-		<div className={classes.usersItems}>
-			<button onClick={getUsers}>Get Users</button>
-			{props.usersData.map(user => <div className={classes.userItem} key={user.id} >
-				<div className={classes.avatar}>
-					<img src={user.photos.small != null ? user.photos.small : searchUserAvatar} alt="" />
-				</div>
-				{user.followed
-					? <button onClick={() => { props.unfollow(user.id) }} className={classes.follow}>Unfollow</button>
-					: <button onClick={() => { props.follow(user.id) }} className={classes.follow}>Follow</button>}
-				<div className={classes.userInfo}>
-					<div className={classes.fullName}>{user.name}</div>
-					<div className={classes.age}>{'user.age'}</div>
-					<div className={classes.country}>{'user.location.countryName'}</div>
-					<div className={classes.city}>{'user.location.cityName'}</div>
-					<div className={classes.status}>{user.status}</div>
-				</div>
-			</div>)}
-		</div>
-	);
-};
+	render() {
+		return (
+			<div className={classes.usersItems} >
+				<button onClick={this.getUsers}>Get Users</button>
+				{this.props.usersData.map(user => <div className={classes.userItem} key={user.id} >
+					<div className={classes.avatar}>
+						<img src={user.photos.small != null ? user.photos.small : searchUserAvatar} alt="" />
+					</div>
+					{user.followed
+						? <button onClick={() => { this.props.unfollow(user.id) }} className={classes.follow}>Unfollow</button>
+						: <button onClick={() => { this.props.follow(user.id) }} className={classes.follow}>Follow</button>}
+					<div className={classes.userInfo}>
+						<div className={classes.fullName}>{user.name}</div>
+						<div className={classes.age}>{'user.age'}</div>
+						<div className={classes.country}>{'user.location.countryName'}</div>
+						<div className={classes.city}>{'user.location.cityName'}</div>
+						<div className={classes.status}>{user.status}</div>
+					</div>
+				</div>)}
+			</div >
+		);
+	}
+}
 
 export default SearchUsersItems;
