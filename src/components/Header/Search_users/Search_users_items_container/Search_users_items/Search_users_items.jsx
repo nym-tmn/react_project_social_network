1a34c1c61bf0056246d884798330e1161bfddbf0
nym-tmn <nymtmn8690@gmail.com>
2023-03-17 01:20:33 +0500

    Add index.html to the root of the project

--- src/components/Header/Search_users/Search_users_items_container/Search_users_items/Search_users_items.jsx
+++ src/components/Header/Search_users/Search_users_items_container/Search_users_items/Search_users_items.jsx
@@ -15,24 +15,29 @@ class SearchUsersItems extends React.Component {
 
 	render() {
 		return (
-			<div className={classes.usersItems} >
-				{/* <button onClick={this.getUsers}>Get Users</button> */}
-				{this.props.usersData.map(user => <div className={classes.userItem} key={user.id} >
-					<div className={classes.avatar}>
-						<img src={user.photos.small != null ? user.photos.small : searchUserAvatar} alt="" />
-					</div>
-					{user.followed
-						? <button onClick={() => { this.props.unfollow(user.id) }} className={classes.follow}>Unfollow</button>
-						: <button onClick={() => { this.props.follow(user.id) }} className={classes.follow}>Follow</button>}
-					<div className={classes.userInfo}>
-						<div className={classes.fullName}>{user.name}</div>
-						<div className={classes.age}>{'user.age'}</div>
-						<div className={classes.country}>{'user.location.countryName'}</div>
-						<div className={classes.city}>{'user.location.cityName'}</div>
-						<div className={classes.status}>{user.status}</div>
-					</div>
-				</div>)}
-			</div >
+			<div>
+				<span>1</span>
+					<span>2</span>
+					<span>3</span>
+					<span>4</span>
+				<div className={classes.usersItems} >
+					{this.props.usersData.map(user => <div className={classes.userItem} key={user.id} >
+						<div className={classes.avatar}>
+							<img src={user.photos.small != null ? user.photos.small : searchUserAvatar} alt="" />
+						</div>
+						{user.followed
+							? <button onClick={() => { this.props.unfollow(user.id) }} className={classes.follow}>Unfollow</button>
+							: <button onClick={() => { this.props.follow(user.id) }} className={classes.follow}>Follow</button>}
+						<div className={classes.userInfo}>
+							<div className={classes.fullName}>{user.name}</div>
+							<div className={classes.age}>{'user.age'}</div>
+							<div className={classes.country}>{'user.location.countryName'}</div>
+							<div className={classes.city}>{'user.location.cityName'}</div>
+							<div className={classes.status}>{user.status}</div>
+						</div>
+					</div>)}
+				</div >
+			</div>
 		);
 	}
 }
