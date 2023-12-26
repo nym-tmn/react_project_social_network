ff474b6d28f1514bcaae3c47d9ec17e3a1463c57
nym-tmn <nymtmn8690@gmail.com>
2023-12-26 21:57:16 +0500

    Deleted form for send posts and created same new form with the help react-hook useForm

--- src/components/Profile/Environment/My_posts/Enter_posts/Enter_posts.tsx
+++ src/components/Profile/Environment/My_posts/Enter_posts/Enter_posts.tsx
@@ -1,43 +1,20 @@
 import React from 'react';
-import { Form } from 'react-final-form';
 
-import { EnterPostsPropsType } from './Enter_posts_container';
-import EnterPostsForm from './Enter_posts_form/Enter_posts_form';
+import EnterPostForm from './Enter_posts_form/Enter_post_form';
 
 import classes from './Enter_posts.module.css';
 
-export type PostFormDataType = {
-	newPostText: string
+export type EnterPostsType = {
+	addPost: (newPostText: string) => void
 }
 
-const EnterPosts: React.FC<EnterPostsPropsType> = (props) => {
-
-	const onAddPost = (postFormData: PostFormDataType) => {
-
-		props.addPost(postFormData.newPostText);
-
-	};
+const EnterPosts: React.FC<EnterPostsType> = (props) => {
 
 	return (
 		<div className={classes.enterPosts}>
 			<div className={classes.iconAvatar}>
 			</div>
-			<Form onSubmit={onAddPost}
-				render={({
-					handleSubmit,
-					values,
-					// form,
-					submitting,
-					pristine,
-				}) => (
-					<EnterPostsForm
-						handleSubmit={handleSubmit}
-						values={values}
-						// form={form}
-						submitting={submitting}
-						pristine={pristine}
-					/>
-				)} />
+			<EnterPostForm addPost={props.addPost}/>
 		</div>
 	);
 };
---
+++ src/components/Profile/Environment/My_posts/Enter_posts/Enter_posts_form/Enter_post_form.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import classes from './Enter_post_form.module.css';
+import { EnterPostsType } from '../Enter_posts';
+
+export type PostFormDataType = {
+	newPostText: string
+}
+
+const EnterPostForm: React.FC<EnterPostsType> = (props) => {
+
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm<PostFormDataType>({});
+
+	const onAddPost: SubmitHandler<PostFormDataType> = (postFormData: PostFormDataType) => {
+
+		props.addPost(postFormData.newPostText);
+		reset();
+
+	};
+
+	return (
+			<form
+				className={classes.enterPostsForm}
+				onSubmit={handleSubmit(onAddPost)}>
+				{errors.newPostText?.message && <div className={classes.error}>{errors.newPostText?.message}</div>}
+				<textarea
+					className={classes.whatIsNew}
+					placeholder={"What's new?"}
+					{...(register('newPostText', {
+						maxLength: {
+							value: 5000,
+							message: 'Max length is 5000 symbols',
+						},
+					}))}>
+				</textarea>
+				<button className={classes.buttonSend}>Send</button>
+			</form>
+	);
+};
+
+export default EnterPostForm;
