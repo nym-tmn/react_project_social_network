7d2bc90f009b5035fe78959f193526e87b961a2f
nym-tmn <nymtmn8690@gmail.com>
2024-09-26 21:19:49 +0500

    Moved images

--- src/redux/dialogs_page_reducer.ts
+++ src/redux/dialogs_page_reducer.ts
@@ -13,12 +13,12 @@ const initialState = {
 		{ id: 3, messageText: 'I am going for a walk. Are you with me? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, sequi maxime adipisci incidunt cumque ratione corporis recusandae, iure nihil quasi optio nobis autem maiores atque reiciendis. Debitis ut, voluptate neque dolor laborum est distinctio assumenda eum, quaerat nesciunt repellendus expedita provident culpa vero veniam repellat. Magnam quo vitae reiciendis dignissimos.' },
 	] as Array<MessagesDataType>,
 	dialogItemData: [
-		{ id: 1, name: 'Robert_lastName', avatar: require('../components/images/dialog_avatar.png') },
-		{ id: 2, name: 'Sophia_lastName', avatar: require('../components/images/dialog_avatar.png') },
-		{ id: 3, name: 'Alex_lastName', avatar: require('../components/images/dialog_avatar.png') },
-		{ id: 4, name: 'Andrew_lastName', avatar: require('../components/images/dialog_avatar.png') },
-		{ id: 5, name: 'John_lastName', avatar: require('../components/images/dialog_avatar.png') },
-		{ id: 6, name: 'Mary_lastName', avatar: require('../components/images/dialog_avatar.png') },
+		{ id: 1, name: 'Robert_lastName', avatar: require('../assets/images/dialog_avatar.png') },
+		{ id: 2, name: 'Sophia_lastName', avatar: require('../assets/images/dialog_avatar.png') },
+		{ id: 3, name: 'Alex_lastName', avatar: require('../assets/images/dialog_avatar.png') },
+		{ id: 4, name: 'Andrew_lastName', avatar: require('../assets/images/dialog_avatar.png') },
+		{ id: 5, name: 'John_lastName', avatar: require('../assets/images/dialog_avatar.png') },
+		{ id: 6, name: 'Mary_lastName', avatar: require('../assets/images/dialog_avatar.png') },
 	] as Array<DialogItemDataType>,
 };
 
